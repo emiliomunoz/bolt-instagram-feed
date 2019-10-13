58cb193d48b315f9a792cb430aec0d866617886e
Emilio Muñoz <emilio@startnet.co>
2019-10-13 03:19:31 -0300

    Added ApcuCache.

--- src/InstagramFeedExtension.php
+++ src/InstagramFeedExtension.php
@@ -4,6 +4,7 @@ namespace Bolt\Extension\emiliomunoz\InstagramFeed;
 
 use Bolt\Extension\SimpleExtension;
 use Vinkla\Instagram\Instagram;
+use Doctrine\Common\Cache\ApcuCache;
 
 /**
  * InstagramFeed extension class.
@@ -28,10 +29,26 @@ class InstagramFeedExtension extends SimpleExtension
      */
     public function instagramTwigFunction(array $params = null)
     {
+        $data = "";
+        if($this->getConfig()["cache_enabled"]) {
+            $cacheDriver = new ApcuCache();
+            $data = $cacheDriver->fetch("instagramMedia");
+
+            if (empty($data)) {
+                $data = $this->getInstagramData($params);
+                $cacheDriver->save("instagramMedia", $data, $this->getConfig()["cache_ttl"]);
+            }
+        }
+        else {
+            $data = $this->getInstagramData($params);
+        };
+
+        return $data;
+    }
+
+    private function getInstagramData($params){
         $accessToken = $this->getConfig()["access_token"];
         $instagram = new Instagram($accessToken);
-
-        $data = $instagram->media($params);
-        return $data;
+        return $instagram->media($params);
     }
 }
