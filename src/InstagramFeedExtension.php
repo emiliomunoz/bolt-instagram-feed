ae51bfa3152656b88506745a0bf0acd86925acec
Emilio Muñoz <emilio@startnet.co>
2019-10-12 21:18:52 -0300

    Initial commit

---
+++ src/InstagramFeedExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\emiliomunoz\InstagramFeed;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * InstagramFeed extension class.
+ *
+ * @author Emilio Muñoz Monge <emiliomunozonge@gmail.com>
+ */
+class InstagramFeedExtension extends SimpleExtension
+{
+}
